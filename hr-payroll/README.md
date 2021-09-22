22257d58237a41a200e9725ceaae1a31d4108e08
Welington <welington_zanon@hotmail.com>
2021-09-22 16:31:52 -0300

    Feign client.

---
+++ hr-payroll/README.md
@@ -0,0 +1,13 @@
+## Important confi
+
+If use STS (spring tool suite) in this project hr-payroll, to use feign clients set POM.xml 
+
+~~~ 
+	<parent>
+		<groupId>org.springframework.boot</groupId>
+		<artifactId>spring-boot-starter-parent</artifactId>
+		<version>2.3.4.RELEASE</version>
+		<relativePath/> <!-- lookup parent from repository -->
+	</parent>
+~~~
+
--- hr-payroll/src/main/java/com/wz/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/wz/payroll/services/PaymentService.java
@@ -1,36 +1,26 @@
 package com.wz.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.wz.payroll.entities.Payment;
 import com.wz.payroll.entities.Worker;
+import com.wz.payroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	private final RestTemplate restTemplate;
+		
+	private final WorkerFeignClient workerFeignClient;
 	
 	@Autowired
-	public PaymentService(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
+	public PaymentService(WorkerFeignClient workerFeignClient) {
+		this.workerFeignClient = workerFeignClient;
 	}
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
 		
 		
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
